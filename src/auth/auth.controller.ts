dee1b23724c5fa2830c972861a1f6f5c04c45ede
Gerardo Miranda <gerardodev25@gmail.com>
2026-04-07 18:57:21 -0400

    feat: add auth DTOs and update auth endpoints
    - Created LoginUserDto and RegisterUserDto for validation
    - Updated auth controller to use @Body decorator with DTOs
    - Added proper payload handling for register and login endpoints
    - Integrated DTOs with NATS client communication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Inject } from '@nestjs/common';
 import { NATS_SERVICE } from 'src/config';
 import { ClientProxy } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {}).pipe(
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
@@ -20,8 +21,8 @@ export class AuthController {
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {}).pipe(
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
